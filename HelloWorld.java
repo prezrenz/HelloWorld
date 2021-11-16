8bc5b1b1157933f045e894a25015e1322784ac26
prezrenz <73829697+prezrenz@users.noreply.github.com>
2021-11-17 02:35:39 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+	public static void printer()
+	{
+		System.out.println("Print this");
+	}
+	public static void main(String []args)
+	{
+		System.out.println("Hello, World!");
+		printer();
+	}
+}
