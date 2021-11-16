0ed13f3e28b73019cb05e7670141c0688166e3b0
prezrenz <perezrenjie20@gmail.com>
2021-11-17 02:53:41 +0800

    Remove printer()

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,7 @@
 public class HelloWorld
 {
-	public static void printer()
-	{
-		System.out.println("Print this");
-	}
 	public static void main(String []args)
 	{
 		System.out.println("Hello, World!");
-		printer();
 	}
 }
